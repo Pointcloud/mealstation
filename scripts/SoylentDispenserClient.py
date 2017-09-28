58daaa3e72ab19609e4a4e419fa9f5383e0fdbb4
Rob Roberts <rroberts@gmail.com>
2017-09-28 06:27:27 -0500

    added soylent dispenser logic

---
+++ scripts/SoylentDispenserClient.py
@@ -0,0 +1,20 @@
+import system
+import time
+import rospy
+from servosix import ServoSix
+
+def handle_dispense_soylent(req):
+    ss.set_servo(6, 0)
+    time.sleep(1)
+    ss.set_servo(6, 120)
+    return True
+
+def dispense_solyent_server():
+    rospy.init_node('dispense_solyent_server')
+    s = rospy.Service('dispense_soylent', DispenseSoylent, handle_dispense_soylent)
+    ss = ServoSix()
+    print "Soylent Dispenser service active"
+    rospy.spin()
+
+if __name__ == "__main__":
+    dispense_solyent_server()
