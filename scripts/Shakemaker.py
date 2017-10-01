1eb955f177c2b40bbbe60badd61b1e2406f94679
Rob Roberts <rroberts@gmail.com>
2017-10-01 06:44:54 -0500

    initial commit of shakemaker

---
+++ scripts/Shakemaker.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import rospy
+from mealstation.srv import *
+
+def dispense_soylent():
+    rospy.wait_for_service('dispense_soylent')
+    try:
+        dispense_soylent = rospy.ServiceProxy('dispense_soylent', PowderDispenser)
+        resp1 = dispense_soylent(1)
+        return resp1.sum
+    except rospy.ServiceException, e:
+        print "Service call failed: %s"%e
+
+if __name__ == "__main__":
+    dispense_soylent()
